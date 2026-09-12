74a4fef26db23038ec1f11386bb72b89fbf75e05
PedroSequeira156 <127869624+PedroSequeira156@users.noreply.github.com>
2026-09-12 23:44:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const QUESTION_BANKS = [
   {
     topic: "RIEAM",
     subtopic: "Balões",
-    path: "questions/RIEAM/Balões/questions.json"
+    path: "questions/RIEAM/Baloes/questions.json"
   },
   {
     topic: "RIEAM",
